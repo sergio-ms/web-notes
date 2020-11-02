2838d31c5874cf25a09f9a0213a98d9a444dbfd2
sergio <moyano.serrano@gmail.com>
2020-11-02 18:53:49 +0100

    First commit

---
+++ app/src/components/Notes.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="Notes">
+    <addNote @add-note="addNote" />
+    <div :key="note.id" v-for="note in notes">
+      <noteItem v-bind:note="note" v-on:del-note="deleteNote" />
+    </div>
+  </div>
+</template>
+<script>
+import NoteItem from "@/components/NoteItem";
+import AddNote from "@/components/AddNote";
+import axios from "axios";
+export default {
+  name: "Notes",
+  components: {
+    NoteItem,
+    AddNote
+  },
+  data() {
+    return {
+      notes: []
+    };
+  },
+  methods: {
+    deleteNote(id) {
+      axios
+        .delete("http://localhost:5000/api/v1/note/" + id)
+        .then(res =>
+          this.notes.push(
+            (this.notes = this.notes.filter(note => note.id !== res.data.id))
+          )
+        )
+        .catch(err => console.log(err));
+    },
+    addNote(newNote) {
+      const { id, name, content } = newNote;
+      console.log(newNote);
+      axios
+        .post("http://localhost:5000/api/v1/notes", {
+          id,
+          name,
+          content
+        })
+        .then(res => this.notes.push(res.data))
+        .catch(err => console.log(err));
+    }
+  },
+  created() {
+    axios
+      .get("http://localhost:5000/api/v1/notes")
+      .then(res => (this.notes = res.data))
+      .catch(err => console.log(err));
+  }
+};
+</script>
+<style scoped></style>
